77869cb7055c0e33ed9d4656248703bf73c14eb7
alexey.tarasenkov <uran-2010@mail.ru>
2020-08-14 14:43:10 +0300

    Add check

--- fc-ee/flowershop/src/main/resources/SQL/v3__order.sql
+++ fc-ee/flowershop/src/main/resources/SQL/v3__order.sql
@@ -4,7 +4,7 @@ orderCreateDate DATE,
 orderCompleteDate DATE,
 userId long,
 total decimal,
-status nvarchar(10),
+status nvarchar(10) check (status in ('CREATED', 'SENT', 'COMPLETED')),
 FOREIGN KEY (userId) REFERENCES Users(id));
 
 --INSERT into ORDERS values(0, '2020-08-05 10:00:00', null, 0, '500', 'CREATED');
